9374109656dabcc0726822ab4303c3f6bac31d2f
user-penguin <kobzev96@gmail.com>
2020-02-16 14:11:43 +0700

    backside: add controller (json response)

---
+++ backside/src/Controller/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class BaseController extends AbstractController
+{
+    /**
+     * Test controller
+     *
+     * @return Response
+     */
+    public function index() : Response
+    {
+        return $this->json(
+            [
+                'name' => [
+                    'Dmitry',
+                    'Petr',
+                ],
+                'action' => 'nofap',
+            ]
+        );
+    }
+}
