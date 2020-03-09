1d59aabdfbb9cebc3f912858cfee5e73f88da97d
xayctoff <petro310897@gmail.com>
2020-03-09 20:17:20 +0700

    frontside: add i18n, update shared module

--- frontside/src/app/shared/shared.module.ts
+++ frontside/src/app/shared/shared.module.ts
@@ -1,30 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 @NgModule({
     declarations: [NavbarComponent],
     imports: [
         CommonModule,
         FlexLayoutModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule
-    ],
-    exports: [
-        FlexLayoutModule,
+        HttpClientModule,
         MatButtonModule,
         MatIconModule,
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
-        NavbarComponent
+        TranslateModule.forRoot({
+            defaultLanguage: 'ru',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
+    ],
+    exports: [
+        FlexLayoutModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        NavbarComponent,
+        TranslateModule
     ]
 })
 export class SharedModule {
+}
+
+//  Required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(http);
 }
