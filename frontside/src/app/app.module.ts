f08077aa35f8088760b92db8979a109cac6e785a
xayctoff <petro310897@gmail.com>
2020-03-09 14:19:34 +0700

    frontside: add navbar component & shared module, fix files code style, refactor styles

--- frontside/src/app/app.module.ts
+++ frontside/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './pages/main/main.component';
@@ -10,7 +12,9 @@ import { MainComponent } from './pages/main/main.component';
         MainComponent
     ],
     imports: [
-        BrowserModule
+        BrowserAnimationsModule,
+        BrowserModule,
+        SharedModule
     ],
     providers: [],
     bootstrap: [AppComponent]
