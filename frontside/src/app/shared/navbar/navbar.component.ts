9d55b619f97db152fdc04b9b55fd93b7bc6fdcca
xayctoff <petro310897@gmail.com>
2020-03-09 15:00:31 +0700

    frontside: implement responsive navbar

--- frontside/src/app/shared/navbar/navbar.component.ts
+++ frontside/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from "@angular/material/sidenav";
 
 @Component({
     selector: 'app-navbar',
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+    @ViewChild('sidenav') _sidenav: MatSidenav;
+
     constructor() {
     }
 
     ngOnInit(): void {
+    }
+
+    sidenavToggle(): void {
+        this._sidenav.toggle().then();
     }
 
 }
